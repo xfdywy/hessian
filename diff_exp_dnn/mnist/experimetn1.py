87ebae1286abc71713307724de9aa1ee7ac1a908
xfdywy <15118424@bjtu.edu.cn>
2017-06-27 08:50:19 +0800

    1

--- diff_exp_dnn/mnist/experimetn1.py
+++ diff_exp_dnn/mnist/experimetn1.py
@@ -38,8 +38,8 @@ dis =[]
 
 temp_step = 0
 
-
-file_name =  '_'.join(model.info.values())
+file_index = '_1'
+file_name =  '_'.join(model.info.values())+file_index
 
 
 printoutfile = open(file_name + '_printout.txt','w')
@@ -102,7 +102,7 @@ for ii in range(10000000):
         temp_step += 1
         
 
-        if  temp_step >5 and  np.mean(train_loss[-5:]) -train_loss[-1]  < (model.lr/100.0 ):
+        if  temp_step >5 and  np.mean(train_loss[-5:]) -train_loss[-1]  < (model.lr/1000.0 ) and model.lr>1e-9:
             model.lr = model.lr / 10.0
             temp_step = 0
             print('learning rate decrease to ', model.lr)
